b89f3b17aa9451dd66c6cf522c6df6eac4135860
GonzalezNacho <nacho.ldm63@gmail.com>
2022-05-12 22:57:47 -0300

    enviando un json

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app =express();
 const morgan = require('morgan');
 
+//settings
+app.set('port', process.env.PORT || 3000);
+
 //middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));//permite leer datos desde los formularios (input)
+app.use(express.json()); //permite usar json
+
+//routes
+app.get('/',(req,res)=> {
+    res.json({"Title":"Hello Word"});
+})
 
 //starting the server
-app.listen(3000, ()=>{
-    console.log(`Server on port ${3000}`)
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`)
 });
